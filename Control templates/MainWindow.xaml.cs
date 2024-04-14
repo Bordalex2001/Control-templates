19143407a671b7a1e803beea23f56cf9f21b93f4
Bord_bf73@student.itstep.org <Bord_bf73@student.itstep.org>
2024-04-14 23:07:10 +0300

    Add project files.

---
+++ Control templates/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Control_templates
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void logInBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string username = usernameField.Text;
+            string password = passwordField.Password;
+
+            // Здесь можно добавить логику проверки имени пользователя и пароля
+            if (username == "admin" && password == "password")
+            {
+                MessageBox.Show("Login successful!");
+                // Здесь можно открыть новое окно или выполнить другие действия после успешной аутентификации
+            }
+            else
+            {
+                MessageBox.Show("Incorrect username or password. Please try again.");
+            }
+        }
+
+        private void cancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
